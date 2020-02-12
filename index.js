256907e98074150cc98d568ab30607a2fd618446
Armadillo <ajknapp2015@gmail.com>
2020-02-12 14:13:33 -0800

    Implemeted github api. Gets user image url and email (email has been null for every user tested

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const https = require('https');
+const axios = require('axios');
 
-let username = 'andrewjknapp';
 
-let options = {
-    host: 'https://api.github.com',
-    path: `/users/:${username}`,
-    headers: {"Authorization": 'd2b87e3d4c9a621c9286bf5aca76bf151d214a43'}
-}
 
-//key d2b87e3d4c9a621c9286bf5aca76bf151d214a43
-https.get(`https://api.github.com/users/:${username}`, options, (resp) => {
-  let data = '';
+inquirer.prompt({
+      type: 'input',
+      message: 'Enter your GitHub Username:',
+      name: 'username'
+}).then(function({ username }) {
+  const queryUrl = `https://api.github.com/users/${username}`;
+  let email;
+  let userImage;
 
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
+  axios
+  .get(queryUrl)
+  .then(function(res) {
+    email = res.data.email;
+    userImage = res.data.avatar_url;
+  })
 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
-  });
+  inquirer.prompt([
+    {
+        type: 'input',
+        message: 'Project Title:',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Description:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Table of Contets (separated by a ,)',
+        name: 'contents'
+    }
+  ]).then(response => {
+      console.log(response);
+  })
+  
+})
 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
 
-// inquirer.prompt([
-//     {
-//         type: 'input',
-//         message: 'Project Title:',
-//         name: 'title'
-//     },
-//     {
-//         type: 'input',
-//         message: 'Description:',
-//         name: 'description'
-//     },
-//     {
-//         type: 'input',
-//         message: 'Table of Contets (separated by a ,)',
-//         name: 'contents'
-//     }
-// ]).then(response => {
-//     console.log(response);
-// })
