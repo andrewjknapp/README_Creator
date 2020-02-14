26c3e7623c02c1d5d441b5b71e2d6721b4849a06
Armadillo <ajknapp2015@gmail.com>
2020-02-13 16:50:48 -0800

    Now produces a README.md rather than a readme.md

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ inquirer.prompt({
     fileContents += `If you have any questions about the repo, open an issue or contact [${username}](${userProfile}) directly at ${email}`;
     
 
-      fs.writeFile('readme.md', fileContents, function(err) {
+      fs.writeFile('README.md', fileContents, function(err) {
         if (err) {
           throw err;
         } else {
